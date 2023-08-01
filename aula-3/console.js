517e63608366c327348bccc2bd843fa417da11d5
Gabriel Toledo <gabriel.toledo2102@gmail.com>
2023-08-01 03:06:18 -0300

    Terceira Aula

---
+++ aula-3/console.js
@@ -0,0 +1,12 @@
+// log -> registro que será armazenado
+const minhaVar = true;
+
+console.log(123);
+console.log("Olá");
+
+// tratamento de erro! error
+if(minhaVar === false){
+    console.error("Deu erro!");
+} else {
+    console.log(minhaVar);
+}
---
+++ aula-3/script.js
@@ -0,0 +1,6 @@
+//tipagem dinamica
+//untyped -> Não precisamos declarar variáveis.
+
+let minhaVar = 567;
+minhaVar = "texto";
+minhaVar = true;
